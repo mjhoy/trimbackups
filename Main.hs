fdacce8533e49366b4437994a90691ef4aa6407e
Michael Hoy <mjh@mjhoy.com>
2016-09-20 23:36:20 -0500

    first draft

--- Main.hs
+++ Main.hs
@@ -1,5 +1,90 @@
 module Main where
 
+import Options.Applicative
+import Control.Monad
+import System.IO
+import System.Exit
+import System.Directory
+import Data.Time
+import System.Posix.Files
+import Data.List
+
+import Trimbackups.Trimmer
+
+data Opts = Opts { optsPath :: String
+                 , optsConfirm :: Bool
+                 , optsDryRun  :: Bool }
+            deriving (Show)
+
+opts :: Parser Opts
+opts = Opts
+  <$> (strArgument
+       (metavar "PATH"
+     <> help "path to the backup directory"))
+  <*> (switch
+       (short 'y'
+     <> help "Delete files without confirmation"))
+  <*> (switch
+       (long "dry-run"
+     <> help "Print a summary of what would be deleted"))
+
+prog :: ParserInfo Opts
+prog = info (helper <*> opts)
+         (fullDesc
+       <> progDesc "trim backups in PATH"
+       <> header "trimbackups -- prune backup files")
+
+exitErr :: String -> IO ()
+exitErr msg = do
+  hPutStrLn stderr msg
+  exitFailure
+
+-- human readable file size
+humanSize :: Real a => a -> [Char]
+humanSize size = humanSize' (realToFrac size) ["Bytes", "Kb", "Mb", "Gb", "Tb"]
+  where
+    humanSize' x bases
+        | x >= 1024 = humanSize' (x / 1024) (tail bases)
+        | otherwise = (show x) ++ " " ++ (head bases)
+
 main :: IO ()
 main = do
-  putStrLn "hello, world!"
+  (Opts path confirm dry) <- execParser prog
+
+  exists <- doesDirectoryExist path
+
+  unless exists $ exitErr $ "path " ++ path ++ " is not a directory"
+
+  now <- getCurrentTime
+
+  trim <- filesToTrim now path
+
+  case dry of
+
+    True -> do
+      putStrLn "dry run, not doing anything...\n"
+      hFlush stdout
+
+      putStrLn "files to trim:"
+      forM_ trim $ \(totrim, _) -> do
+        putStrLn totrim
+
+      let sumSize = foldl' (\size (_, status) -> size + fileSize status) 0 trim
+      putStrLn $ "\ntotal space to remove: " ++ (humanSize sumSize)
+
+    False -> do
+
+      let n = length trim
+          w = if n == 1 then "file" else "files"
+
+      when (n < 1) $ do
+        exitSuccess
+
+      unless confirm $ do
+        putStrLn $ (show n) ++ " " ++ w ++ " will be removed. Continue? [y/n]"
+        inp <- getChar
+        when (inp /= 'y') $ do
+          exitErr "User aborted"
+
+      forM_ trim $ \(totrim, _) -> do
+        putStrLn $ "rm " ++ totrim
---
+++ Trimbackups/Trimmer.hs
@@ -0,0 +1,79 @@
+module Trimbackups.Trimmer (filesToTrim) where
+
+import System.Directory
+import System.FilePath.Posix ((</>))
+import Data.Time
+import Data.Time.Clock.POSIX
+import System.Posix.Files
+import Control.Monad
+import Data.List
+import Data.Tuple.Extra
+
+modificationTimeUTC :: FileStatus -> UTCTime
+modificationTimeUTC = posixSecondsToUTCTime . modificationTimeHiRes
+
+-- Return absolute paths to the contents of `dir`.
+getAbsoluteDirectoryContents :: FilePath -> IO [FilePath]
+getAbsoluteDirectoryContents dir = do
+  files <- getDirectoryContents dir
+  return $ map (\f -> dir </> f) files
+
+-- Return absolute paths to files in `dir`.
+getAbsoluteDirectoryFiles :: FilePath -> IO [FilePath]
+getAbsoluteDirectoryFiles dir =
+  getAbsoluteDirectoryContents dir >>= filterM doesFileExist
+
+type FileMTime = (UTCTime, FilePath, FileStatus)
+
+foldFn :: (Maybe UTCTime, UTCTime, [FileMTime]) -> FileMTime -> (Maybe UTCTime, UTCTime, [FileMTime])
+foldFn (prevTime , now, acc) cur = (prevTime', now, acc')
+  where
+    curTime = fst3 cur
+    keep = case prevTime of
+
+      -- This is the most recent backup, of course we keep it.
+      Nothing -> True
+
+      -- Otherwise, compare with the previously kept backup.
+      Just prevTimeJ
+
+          -- after 2 months, weekly backups.
+          | scope > (2 * month) -> diff > week
+
+          -- after 2 weeks, daily backups.
+          | scope > (2 * week) -> diff > day
+
+          -- after a day, 6-hour backups.
+          | scope > day -> diff > (6 * hour)
+
+          -- otherwise, keep everything.
+          | otherwise -> True
+
+        where diff = diffUTCTime prevTimeJ curTime
+              scope = diffUTCTime now prevTimeJ
+              hour = 60 * 60
+              day = hour * 24
+              week = day * 7
+              month = day * 30
+
+    (prevTime', acc') = if keep then (Just curTime, acc) else (prevTime, cur : acc)
+
+pairFileWithMTime :: FilePath -> IO FileMTime
+pairFileWithMTime path = do
+  status <- getFileStatus path
+  let mtime = modificationTimeUTC status
+  return (mtime, path, status)
+
+sortFstDesc :: [FileMTime] -> [FileMTime]
+sortFstDesc = sortBy (\a b -> (fst3 b) `compare` (fst3 a))
+
+filesToTrim :: UTCTime ->       -- current time
+               FilePath ->      -- directory to look in
+               IO [(FilePath, FileStatus)]    -- files we should delete
+filesToTrim now backupDir = do
+  filesAndTimes <- getAbsoluteDirectoryFiles backupDir >>=
+                   mapM pairFileWithMTime >>=
+                   return . sortFstDesc
+  let removeFiles = foldl' foldFn (Nothing, now, []) filesAndTimes
+  let files = map (\x -> (snd3 x, thd3 x)) (thd3 removeFiles)
+  return files
